a24325f81751d5d80d2284a0794a16969f4e173e
keuheam <76203739+keuheum@users.noreply.github.com>
2021-05-26 10:16:04 +0900

    Update get_ip.py

--- get_ip.py
+++ get_ip.py
@@ -4,7 +4,7 @@ import re
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(("pwnbit.kr", 443))
-print(f"내부 IP : {sock.getsockname()[0]}")
+print(f"inside IP : {sock.getsockname()[0]}")
 
 req = requests.get("http://ipconfig.kr")
-print("외부 IP : " + str(re.search(r'IP Address : (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', req.text)[1]))
+print("outside IP : " + str(re.search(r'IP Address : (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', req.text)[1]))
